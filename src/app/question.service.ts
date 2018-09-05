aa667956688641ceed396e8adf751da281d52141
amakomitchell <amakomitchell5@gmail.com>
2018-09-05 12:16:20 +0100

    A dynamic-form project using angular 4

---
+++ src/app/question.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+
+import { QuestionDropdown } from './question-dropdown';
+import { QuestionBase } from './question-base';
+import { QuestionTextbox } from './question-textbox';
+
+@Injectable()
+export class QuestionService {
+
+  // Todo: get from a remote source of question metadata
+  // Todo: make asynchronous
+
+  getQuestions() {
+    
+    let questions: QuestionBase<any>[] = [
+
+      new QuestionDropdown({
+        key:'brave',
+        label:'Bravery Rating',
+        options: [
+          {key: 'solid', value: 'Solid'},
+          {key: 'great', value: 'Great'},
+          {key: 'good', value: 'Good'},
+          {key: 'unproven', value: 'Unproven'}
+        ],
+        order: 3
+      }),
+
+      new QuestionTextbox({
+        key: 'firstName',
+        label: 'First Name',
+        value: 'Bombasto',
+        required: true,
+        order: 1
+      }),
+
+      new QuestionTextbox({
+        key: 'emailAddress',
+        label: 'Email',
+        type: 'email',
+        order: 2
+      })
+    ];
+
+    return questions.sort((a, b) => a.order - b.order);
+  }
+
+  constructor() { }
+
+}
